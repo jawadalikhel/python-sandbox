e9734778fbb943470129d935707a627ceaf9c335
Jawad Alikhel <jawadalikhel@Jawads-MBP.lan>
2020-09-06 10:33:04 -0500

    stated with pythond list methods

---
+++ index.py
@@ -0,0 +1,13 @@
+list_1 = [1,2,3,4,5];
+print("Original List: ", list_1);
+
+list_1.append("Six");
+print("Append List: ", list_1);
+
+newList = [7,8];
+list_1.extend(newList);
+print("Extend List: ", list_1);
+
+
+
+# tuple_1 = 
